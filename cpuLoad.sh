532456e3df965184ae52dcb0e5dfe89d37e8642a
zounp <19635343+zounp@users.noreply.github.com>
2021-06-17 19:14:30 +0000

    Update cpuLoad.sh

--- cpuLoad.sh
+++ cpuLoad.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
+# Introduction
+# ------------
 # This program is meant to prevent a processor from freezing caused by a too low cpu load
+
+# Manual
+# ------
 # This program is meant to be ran in a Launcher (xfce) in the system tray
+# In case of the default values make sure the tray update period
+# is larger then 2 seconds since stress runs for 2 seconds
+
 
 triggerload=5
 munberofcpus=1
@@ -11,9 +19,6 @@ loadtimecpu=2 # seconds
 usersysload=$(top -b -n 1 | grep "Cpu(s)" | tr ',' '.' | awk '{printf "%0.1f", $2 + $4}')
 
 echo -e "Load:\n$usersysload"
-
-# In case of the default values make sure the tray update period
-# is larger then 2 seconds since stress runs for 2 seconds
 
 # Load the computer for $loadtimecpu seconds (default 2)
 # if the value of loadhigh is below $usersysload (default 5)
