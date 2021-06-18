43ecd51c6f82add2b0edbebaec1b2a943b7916b5
zounp <19635343+zounp@users.noreply.github.com>
2021-06-18 21:23:08 +0000

    Update cpuLoad.sh

--- cpuLoad.sh
+++ cpuLoad.sh
@@ -16,12 +16,12 @@ munberofcpus=1
 loadtimecpu=2 # seconds
 
 # the user load + the systemload according to top
-usersysload=$(top -b -n 1 | grep "Cpu(s)" | tr ',' '.' | awk '{printf "%0.1f", $2 + $4}')
+usersysload=$(top -b -n 1 | grep 'Cpu(s)' | tr ',' '.' | awk '{printf "%0.1f", $2 + $4}')
 
 echo -e "Load:\n$usersysload"
 
 # Load the computer for $loadtimecpu seconds (default 2)
 # if the value of loadhigh is below $usersysload (default 5)
-if [[ $(echo $usersysload | cut -d '.' -f 1) -lt $triggerload ]]; then
+if [[ $(cut -d '.' -f 1 <<< $usersysload) -lt $triggerload ]]; then
   stress --quiet --cpu $munberofcpus --timeout $loadtimecpu
 fi
