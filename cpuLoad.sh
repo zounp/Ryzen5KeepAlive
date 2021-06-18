1635b228fd2756d230497a2876dc96d96c586163
zounp <19635343+zounp@users.noreply.github.com>
2021-06-18 21:31:33 +0000

    Update cpuLoad.sh

--- cpuLoad.sh
+++ cpuLoad.sh
@@ -6,22 +6,22 @@
 
 # Manual
 # ------
-# This program is meant to be ran in a Launcher (xfce) in the system tray
-# In case of the default values make sure the tray update period
-# is larger then 2 seconds since stress runs for 2 seconds
+# This program is meant to be ran in a generic monitor in a xfce panel.
+# In case of the default values make sure the generic monitor update period
+# is larger then 2 seconds since stress runs for 2 seconds.
 
 
 triggerload=5
 munberofcpus=1
-loadtimecpu=2 # seconds
+loadtimecpu=2 # seconds.
 
-# the user load + the systemload according to top
+# the user load + the systemload according to top.
 usersysload=$(top -b -n 1 | grep 'Cpu(s)' | tr ',' '.' | awk '{printf "%0.1f", $2 + $4}')
 
 echo -e "Load:\\n$usersysload"
 
-# Load the computer for $loadtimecpu seconds (default 2)
-# if the value of loadhigh is below $usersysload (default 5)
+# Load the computer for $loadtimecpu seconds (default 2).
+# if the value of loadhigh is below $usersysload (default 5).
 if [[ $(cut -d '.' -f 1 <<< "$usersysload") -lt $triggerload ]]; then
   stress --quiet --cpu $munberofcpus --timeout $loadtimecpu
 fi
